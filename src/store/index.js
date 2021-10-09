baae41af1f5f9413bf45898d4c5f1ddd6f790b05
correctionjs1-3 <tana39754@gmail.com>
2021-10-09 12:39:31 +0900

    課題２完了致しましたのでご確認御願いします

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import firebase from 'firebase'
+import router from '@/router'
 
 Vue.use(Vuex)
 
@@ -8,25 +9,48 @@ export default new Vuex.Store({
   state: {
     username: '',
     email: '',
-    password: ''
+    password: '',
+    LoginEmail: '',
+    LoginPassword: ''
   },
   mutations: {
     registerState(state, payload) {
       state.username = payload.username
       state.email = payload.email
       state.password = payload.password
+    },
+    loginState(state, payload) {
+      state.LoginEmail = payload.LoginEmail
+      state.LoginPassword = payload.LoginPassword
     }
   },
   actions: {
-    register(context, payload) {
+    newRegister(context, payload) {
       firebase
       .auth()
       .createUserWithEmailAndPassword(payload.email, payload.password)
       .then(() => {
         context.commit('registerState', payload)
       })
+      .then(() => {
+        router.push('/about')
+      })
       .catch((e) => {
-        console.error("エラー :", e.message)
+        console.error('エラー :', e.message)
+      })
+    },
+    LoginUser(context, payload) {
+      firebase
+      .auth()
+      .signInWithEmailAndPassword(payload.LoginEmail, payload.LoginPassword)
+      .then(() => {
+        context.commit('loginState', payload)
+      })
+      .then(() => {
+        router.push('/about');
+      })
+      .catch((e) => {
+        console.error('エラー :', e.message)
       })
     }
   },
---
+++ src/views/Login.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1 class="title">ログイン画面</h1>
+    <form>
+      <ul>
+        <li>
+          <label for="email">メールアドレス</label>
+          <input type="email" id="email" placeholder="E-mail" v-model="LoginEmail" />
+        </li>
+        <li>
+          <label for="password">パスワード</label>
+          <input type="password" id="password" placeholder="PassWord" v-model="LoginPassword" />
+        </li>
+      </ul>
+    </form>
+    <button class="button is-info is-outlined" @click="LoginUser">ログイン</button>
+    <br />
+    <router-link to="/">新規登録はこちらから</router-link> 
+  </div>
+</template>
+<script>
+export default {
+  name: 'singin',
+  data() {
+    return {
+      LoginEmail: '',
+      LoginPassword: ''
+    };
+  },
+  methods: {
+    LoginUser() {
+      this.$store.dispatch('LoginUser', {
+        LoginEmail: this.LoginEmail, 
+        LoginPassword: this.LoginPassword
+      })
+    }
+  }
+};
+</script>
